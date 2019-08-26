bf5b1fa0502198cd350e969f84592ae1d0b9cfb5
maddokev108 <10890969@my.uvu.edu>
2019-08-26 17:12:53 -0600

    Created a Silly String (and such)
    added player sprite and a variable script.

---
+++ Unity Things/2D Project 1 Thing/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string name = "Potama Llama";
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(name);
+    }
+
+}
