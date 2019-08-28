f734362ad14dcf62dbc36c528f8740ea53dc5744
maddokev108 <10890969@my.uvu.edu>
2019-08-28 17:05:17 -0600

    Created Some Not-So-Handy Tips
    Added a few more variables to the "Variables" C# script, and put the script into a new folder.

---
+++ Unity Things/2D Project 1 Thing/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string thing = "Potama Llama";
+    int twelve = 12;
+    float kindaTwelve = 12.00000001f;
+    bool right = true;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The thing is called \"" + thing + ".\"");
+        Debug.Log("The number to use when you want a dozen donuts, but the word \"dozen\" is too mainstream: " + twelve);
+        Debug.Log("The number to use when you want twelve donuts, but 12 is too mainstream: " + kindaTwelve);
+        Debug.Log("When it's not false, it's " + right);
+    }
+
+}
--- Unity Things/2D Project 1 Thing/Assets/Scripts/Experiments/types of variables.cs
+++ Unity Things/2D Project 1 Thing/Assets/Scripts/Experiments/types of variables.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Variables : MonoBehaviour
+public class typesofvariables : MonoBehaviour
 {
-    string name = "Potama Llama";
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(name);
+        
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
