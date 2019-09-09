9b7ae1cc3c3015ce7b534090a6074946603c152b
maddokev108 <10890969@my.uvu.edu>
2019-09-09 17:07:27 -0600

    Discovered the 3rd Dimension
    Created a new 3d project.

---
+++ Unity Things/2D Platformer/Assets/Scripts/Assignments/Functions/Bananas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bananas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(bonusBanana("That's right! 5 LB of sugar, for only $99.99!"));
+    }
+
+}
+
+str bonusBanana (str in)
+    {
+        Debug.Log("Distributing bonus...");
+        return (in+" And a FREE banana!");
+    }
