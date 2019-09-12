c84be8a14d347735369d500150eeaff906937c01
maddokev108 <10890969@my.uvu.edu>
2019-09-11 21:20:21 -0600

    Vroomed
    Added a movement script to the bus, and applied RigidBody physics to the bus and obstacles. Gave the boulders some friction.

---
+++ Unity Things/'Create With Code' Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This code makes the vehicle move.
+        transform.Translate(Vector3.forward*Time.deltaTime*40);   // This will move the vehicle forward by 1 unit along the Z axis.
+    }
+}
