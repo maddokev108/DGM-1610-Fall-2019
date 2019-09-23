ef4bb2f559fbdf190035ae92a2202ce0d5fcf346
maddokev108 <10890969@my.uvu.edu>
2019-09-23 16:44:53 -0600

    Completed the Plane Challenge
    ^

--- Unity Things/'Create With Code' Project/Assets/Scripts/PlayerController.cs
+++ Unity Things/'Create With Code' Project/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
+    public float speed = 20f;
+    private float turnSpeed = 40f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //This code makes the vehicle move.
-        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+        //This code controls the vehicle's movement.
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //This code makes the vehicle move
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
