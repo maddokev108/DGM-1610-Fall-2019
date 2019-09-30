c17b5c388b6c8376dc2c4819c1962cf10631a8b6
maddokev108 <10890969@my.uvu.edu>
2019-09-30 16:14:27 -0600

    A Flat New World
    made a project for Bullet Shell

---
+++ Unity Things/Bullet Shell/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 20f;
+    private float turnSpeed = 90f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This code controls the vehicle's movement.
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //This code makes the vehicle move
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
