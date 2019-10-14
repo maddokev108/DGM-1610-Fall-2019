30335b89ff34fa08eb688f62c637f0ba46b38715
maddokev108 <10890969@my.uvu.edu>
2019-10-13 21:25:21 -0600

    Played Fetch
    Completed Challenge #2.

---
+++ Unity Things/Deer & Pizza Game/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastDog = -1f; //initialize lastDog as -1 to allow the player to fire imediately.
+    private float dogTime;
+    private float cooldown = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the time since the last dog was spawned
+        dogTime = Time.time - lastDog;
+
+        // On spacebar press, send dog if the cooldown has finished.
+        if (Input.GetKeyDown(KeyCode.Space) & dogTime >= cooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            //update the timestamp for the most recent fire.
+            lastDog = Time.time;
+        }
+    }
+}
