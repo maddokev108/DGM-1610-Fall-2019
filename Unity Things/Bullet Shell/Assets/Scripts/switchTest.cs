154d1848c4bcd29f2fe794331a5b975ccb0d3d80
maddokev108 <10890969@my.uvu.edu>
2019-10-16 17:08:01 -0600

    Lesson 3.1, 3.2(almost), & An Experiment
    Started experimenting with case statements before class for my Personal Project.
    Completed lesson 3.1 & part of 3.2

---
+++ Unity Things/Bullet Shell/Assets/Scripts/switchTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float button = ( Input.GetAxisRaw("Horizontal") + 1 ) / 2 + 1;
+        if ( button != 0 )
+        {
+            upDown(button);
+            button = 0;
+        }
+    }
+    void upDown(float input){
+        switch (input)
+        {
+            case 1:
+                Debug.Log("up");
+                break;
+            case 2:
+                Debug.Log("down");
+                break;
+            default:
+                Debug.Log("Something went wrong.");
+                break;
+
+        }
+    }
+}
---
+++ Unity Things/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public bool grounded = true;
+    public float gravityModifier = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity = Physics.gravity * gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            playerRb.AddForce(Vector3.up * 12.5f, ForceMode.Impulse);
+            grounded = false;
+        }
+    }
+    private void OnCollisionEnter (Collision collision){
+        grounded = true;
+    }
+}
