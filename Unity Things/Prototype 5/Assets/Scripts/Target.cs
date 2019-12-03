7b684315afa9eab9270adbfc2d34096fd67e0f6d
maddokev108 <10890969@my.uvu.edu>
2019-12-02 19:15:06 -0700

    Prototype 5 & Challenge 5 - Completed
    Completed both Prototype 5 & Challenge 5. I would have done it sooner, but I chose to work on my animation projects over the break (and before it) instead.
    
    I'm going to implement audio/visual effects and UI into my personal project on Wednesday, and possibly add an extra feature to it on Thursday or Friday (if I finished my animation projects by then).

--- Unity Things/Prototype 5/Assets/Scripts/Target.cs
+++ Unity Things/Prototype 5/Assets/Scripts/Target.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody targetRb; //the rigidbody of the object
+    private GameManager gameManager;
+    public ParticleSystem explosionParticle;
+
+
+    private float minSpeed = 12f;
+    private float maxSpeed = 16f;
+
+    private float maxTorque = 10f;
+
+    private float xRange = 4f;
+    private float ySpawnPos = -4f;
+
+    public int pointValue;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown() // When this object is clicked...
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject); // ... Destroy it.
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+            if (gameObject.CompareTag("Bad"))
+            {
+                gameManager.GameOver();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    Vector3 RandomForce() 
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
